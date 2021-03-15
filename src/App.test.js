7778f3a71fc9d78b5dd9cd40d21f05a274072bb2
EC2 Default User <ec2-user@ip-172-31-80-214.ec2.internal>
2021-03-15 20:33:17 +0000

    3 React Component Jest Unit Tests Completed

--- src/App.test.js
+++ src/App.test.js
@@ -3,25 +3,34 @@ import App from './App';
 import Login from './Login';
 import Board from './Board';
 
- test('reset button dissapears', () => {
-   const result = render(<App />);
-   const resetButtonElement = screen.getByText('Reset');
-   expect(resetButtonElement).toBeInTheDocument();
-   fireEvent.click(resetButtonElement);
-   expect(resetButtonElement).not.toBeInTheDocument();
+test('Spectators display properly', () => {
+     const result = render(<App />);
+     const spectatorsElement = screen.getByText('Login');
+     expect(spectatorsElement).toBeInTheDocument();
+     const spectatorsElement2 = document.getElementById('input');
+     fireEvent.change(spectatorsElement2, { target: {value: 'userA' } });
+     fireEvent.click(spectatorsElement);
+     expect(spectatorsElement2).not.toBeInTheDocument();
+     const spectatorsElement3 = screen.getByText('Spectators:');
+     expect(spectatorsElement3).toBeInTheDocument();
+ });
+ 
+ test('Leaderboard', () => {
+     const result = render(<Board player2 = "userA" />);
+     const hideElement = screen.getByText('View/Hide LeaderBoard');
+     expect(hideElement).toBeInTheDocument();
+     fireEvent.click(hideElement);
+     const rankingElement = document.getElementById('LEADERBOARD');
+     expect(rankingElement).not.toBeInTheDocument();
  });
  
  test('Login Button', () => {
      const result = render(<App />);
-     const loginElement = screen.getByText('');
-     
+     const loginElement = screen.getByText('Login');
+     expect(loginElement).toBeInTheDocument();
+     const loginElement2 = document.getElementById('input');
+     fireEvent.change(loginElement2, { target: {value: 'userA' } });
+     fireEvent.click(loginElement);
+     expect(loginElement2).not.toBeInTheDocument();
  });
 
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
